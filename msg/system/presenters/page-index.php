0ae016efa77a3d42ca830bec6d9a74b4d24aed77
gianl <gianl@MANIAC-NB>
2016-12-16 20:11:00 +0100

    Seconda bozza messaggistica

--- msg/system/presenters/page-index.php
+++ msg/system/presenters/page-index.php
@@ -1,77 +1,203 @@
+<?php
+require_once 'session.php';
+
+//setto la pagina attiva
+if (isset($_SERVER['REQUEST_URI'])) {
+    $_SESSION['activePage'] = basename($_SERVER['REQUEST_URI']);
+}
+?>
 <!DOCTYPE html>
 <html lang="it">
-<head>
-   <meta http-equiv="content-Type" content="text/html; charset=UTF-8"/>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <meta name="author" content="Gianluca Tortorella" />
-   <title><?php echo $settings->site_name;?></title>
-   
-    <!--[if lt IE 9]>
-        <script src="./msg/js/html5shiv.min.js"></script>
-    <![endif]-->
-   
-    <link rel="shortcut icon" href="./msg/img/favicon.png" />
-    <?php if (!$settings->direction_rtl): ?>
-      <link rel="stylesheet" type="text/css" href="./msg/css/bootstrap.min.css" />
-      <link rel="stylesheet" type="text/css" href="./msg/css/bootstrap-responsive.min.css" />
-    <?php else: ?>
-      <link rel="stylesheet" type="text/css" href="./msg/css/bootstrap-rtl.min.css" />
-      <link rel="stylesheet" type="text/css" href="./msg/css/bootstrap-responsive-rtl.min.css" />
-    <?php endif;?>
-    <link rel="stylesheet" type="text/css" href="./msg/css/index-page.css" />
-    <script src="./msg/js/jquery.v2.0.2.min.js" type="text/javascript"></script>
-    <script src="./msg/js/fn.extends.js" type="text/javascript"></script>
-    <script src="./msg/js/bootstrap.min.js" type="text/javascript"></script>
-    <script src="./msg/js/system.page-index.js" type="text/javascript"></script>
-    <script type="text/javascript">
-        var filename = "<?php echo basename($_SERVER["SCRIPT_NAME"]);?>";
-    </script>
-</head>
-<body>
+    <head>
+        <meta charset="utf-8" />
+        <title>Brief | Pannello Admin</title>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+        <meta content="Preview page of Metronic Admin Theme #4 for statistics, charts, recent events and reports" name="description" />
+        <meta content="" name="author" />
+        <!-- BEGIN GLOBAL MANDATORY STYLES -->
+        <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" />
+        <link href="assets/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css" />
+        <!-- END GLOBAL MANDATORY STYLES -->
+        <!-- BEGIN PAGE LEVEL PLUGINS -->
+        <link href="assets/global/plugins/bootstrap-daterangepicker/daterangepicker.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/global/plugins/morris/morris.css" rel="stylesheet" type="text/css" />
+        <link href="assets/global/plugins/fullcalendar/fullcalendar.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/global/plugins/jqvmap/jqvmap/jqvmap.css" rel="stylesheet" type="text/css" />
+        <link href="assets/global/plugins/datatables/datatables.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.css" rel="stylesheet" type="text/css" />
+        <link href="assets/global/plugins/bootstrap-fileinput/bootstrap-fileinput.css" rel="stylesheet" type="text/css" />
+        <link href="assets/global/plugins/bootstrap-toastr/toastr.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/global/plugins/bootstrap-daterangepicker/daterangepicker.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/global/plugins/bootstrap-datepicker/css/bootstrap-datepicker3.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/global/plugins/bootstrap-timepicker/css/bootstrap-timepicker.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/global/plugins/bootstrap-datetimepicker/css/bootstrap-datetimepicker.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/global/plugins/clockface/css/clockface.css" rel="stylesheet" type="text/css" />
+        <!-- END PAGE LEVEL PLUGINS -->
+        <!-- BEGIN THEME GLOBAL STYLES -->
+        <link href="assets/global/css/components.min.css" rel="stylesheet" id="style_components" type="text/css" />
+        <link href="assets/global/css/plugins.min.css" rel="stylesheet" type="text/css" />
+        <!-- END THEME GLOBAL STYLES -->
+        <!-- BEGIN PAGE LEVEL STYLES -->
+        <link href="assets/pages/css/profile.min.css" rel="stylesheet" type="text/css" />
+        <!-- END PAGE LEVEL STYLES -->
+        <!-- BEGIN THEME LAYOUT STYLES -->
+        <link href="assets/layouts/layout4/css/layout.min.css" rel="stylesheet" type="text/css" />
+        <link href="assets/layouts/layout4/css/themes/default.min.css" rel="stylesheet" type="text/css" id="style_color" />
+        <link href="assets/layouts/layout4/css/custom.min.css" rel="stylesheet" type="text/css" />
+        <!-- END THEME LAYOUT STYLES -->
+        <link rel="shortcut icon" href="assets/base/img/favicon.png" /> 
+        <script type="text/javascript">
+            // Toastr
 
-<div class="container">
-  <h1><?php echo $settings->site_name;?></h1>
+            function notifyInfo(title, msg) {
+                // Display an info toast with no title
+                toastr.info(msg, title);
+            }
 
-  <div class="alert alert-error" <?php echo (!$alert ? 'style="display: none;"' : ''); ?>>
-    <button type="button" class="close" onclick="$(this).parent().hide(0);">&times;</button>
-    <strong><?php __("warning");?></strong> <span id="alert_text"><?php echo $alert;?></span>
-  </div> 
-  
-  <ul class="nav nav-tabs">
-    <li class="active"><a href="#tab_login" data-toggle="tab"><?php __("login");?></a></li>
-    <?php if ($settings->enabled_register): ?><li><a href="#tab_register" data-toggle="tab"><?php __("register");?></a></li><?php endif; ?>
-  </ul>
- 
-  <div class="tab-content">
-    <div class="tab-pane active" id="tab_login">
-      <form method="post" action="ajax.php" id="login" onsubmit="send_form('login'); return false;">
-          <input type="hidden" name="action" value="login">
-          <h2 class="form-signin-heading"><?php __("please_sign_in");?></h2>
-          <input type="text" name="login" class="input-block-level" placeholder="<?php __("login");?>">
-          <input type="password" name="password" class="input-block-level" placeholder="<?php __("password");?>">
-          <label class="checkbox">
-            <input type="checkbox" name="remember"> <?php __("remember_me");?>
-          </label>
-          <button class="btn btn-large btn-primary" type="submit"><?php __("sign_in");?></button>
-        </form>    
-    </div>
-    
-    <?php if ($settings->enabled_register): ?>  
-    <div class="tab-pane" id="tab_register">
-      <form method="post" action="ajax.php" id="register" onsubmit="send_form('register'); return false;">
-          <input type="hidden" name="action" value="register">
-          <h2 class="form-signin-heading"><?php __("create_member");?></h2>
-          <input type="text" name="login" class="input-block-level" placeholder="<?php __("login");?>">
-          <input type="password" name="password" class="input-block-level" placeholder="<?php __("password");?>">
-          <input type="password" name="password_repeat" class="input-block-level" placeholder="<?php __("password_repeat");?>">
-        
-          <button class="btn btn-large btn-primary" type="submit"><?php __("register");?></button>
-        </form>  
-    </div>
-    <?php endif; ?>
-    
-  </div>
-</div>
+            function notifySuccess(title, msg) {
+                // Display a success toast with no title
+                toastr.success(msg, title);
+            }
 
-</body>
+            function notifyWarning(title, msg) {
+                // Display a warning toast with no title
+                toastr.warning(msg, title);
+            }
+
+            function notifyError(title, msg) {
+                // Display a warning toast with no title
+                toastr.error(msg, title);
+            }
+        </script>
+        <!-- BEGIN MSG --> 
+        <!--[if lt IE 9]>
+            <script src="./msg/js/html5shiv.min.js"></script>
+        <![endif]-->
+
+
+        <link rel="stylesheet" type="text/css" href="./msg/css/index-page.css" />
+        <script src="./msg/js/jquery.v2.0.2.min.js" type="text/javascript"></script>
+        <script src="./msg/js/fn.extends.js" type="text/javascript"></script>
+        <script src="./msg/js/system.page-index.js" type="text/javascript"></script>
+        <script type="text/javascript">
+            var filename = "<?php echo basename($_SERVER["SCRIPT_NAME"]); ?>";
+        </script>
+        <!-- END MSG -->
+    </head>
+    <body class="page-container-bg-solid page-header-fixed page-sidebar-closed-hide-logo">
+        <?php include 'backend-header.php' ?>
+        <!-- BEGIN CONTAINER -->
+        <div class="page-container">
+            <?php include 'backend-sidebar.php' ?>
+            <!-- BEGIN CONTENT -->
+            <div class="page-content-wrapper">
+                <!-- BEGIN CONTENT BODY -->
+                <div class="page-content">
+                    <!-- BEGIN PAGE BASE CONTENT -->
+                    <div class="container">
+                        <ul class="nav nav-tabs">
+                            <li class="active"><a href="#tab_login" data-toggle="tab"><?php __("login"); ?></a></li>
+                            <?php if ($settings->enabled_register): ?><li><a href="#tab_register" data-toggle="tab"><?php __("register"); ?></a></li><?php endif; ?>
+                        </ul>
+
+                        <form method="post" action="./msg/system/presenters/ajax.php" id="login" onsubmit="send_form('login'); return false;">
+                            <input type="hidden" name="action" value="login">
+                            <h2 class="form-signin-heading"><?php __("please_sign_in"); ?></h2>
+                            <input type="text" name="login" class="input-block-level" placeholder="<?php __("login"); ?>">
+                            <input type="password" name="password" class="input-block-level" placeholder="<?php __("password"); ?>">
+                            <label class="checkbox">
+                                <input type="checkbox" name="remember"> <?php __("remember_me"); ?>
+                            </label>
+                            <button class="btn btn-large btn-primary" type="submit"><?php __("sign_in"); ?></button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- BEGIN FOOTER -->
+        <div class="page-footer">
+            <div class="page-footer-inner"> 2016 &copy; Admin Theme By
+                <a target="_blank" href="http://infobasic.com">Infobasic Srl</a> &nbsp;|&nbsp;
+            </div>
+            <div class="scroll-to-top">
+                <i class="icon-arrow-up"></i>
+            </div>
+        </div>
+        <!-- END FOOTER -->
+        <!--[if lt IE 9]>
+        <script src="assets/global/plugins/respond.min.js"></script>
+        <script src="assets/global/plugins/excanvas.min.js"></script> 
+        <script src="assets/global/plugins/ie8.fix.min.js"></script> 
+        <![endif]-->
+        <!-- BEGIN CORE PLUGINS -->
+        <script src="assets/global/plugins/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/js.cookie.min.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/jquery-slimscroll/jquery.slimscroll.min.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/jquery.blockui.min.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js" type="text/javascript"></script>
+        <!-- END CORE PLUGINS -->
+        <!-- BEGIN PAGE LEVEL PLUGINS -->
+        <script src="assets/global/plugins/moment.min.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/bootstrap-daterangepicker/daterangepicker.min.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/morris/morris.min.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/morris/raphael-min.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/counterup/jquery.waypoints.min.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/counterup/jquery.counterup.min.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/amcharts/amcharts/amcharts.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/amcharts/amcharts/serial.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/amcharts/amcharts/pie.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/amcharts/amcharts/radar.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/amcharts/amcharts/themes/light.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/amcharts/amcharts/themes/patterns.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/amcharts/amcharts/themes/chalk.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/amcharts/ammap/ammap.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/amcharts/ammap/maps/js/worldLow.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/amcharts/amstockcharts/amstock.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/fullcalendar/fullcalendar.min.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/horizontal-timeline/horizontal-timeline.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/flot/jquery.flot.min.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/flot/jquery.flot.resize.min.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/flot/jquery.flot.categories.min.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/jquery-easypiechart/jquery.easypiechart.min.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/jquery.sparkline.min.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/jqvmap/jqvmap/jquery.vmap.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.russia.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.world.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.europe.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.germany.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/jqvmap/jqvmap/maps/jquery.vmap.usa.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/jqvmap/jqvmap/data/jquery.vmap.sampledata.js" type="text/javascript"></script>
+        <script src="assets/global/scripts/datatable.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/datatables/datatables.min.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/bootstrap-fileinput/bootstrap-fileinput.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/jquery.sparkline.min.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/bootstrap-toastr/toastr.min.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/bootstrap-daterangepicker/daterangepicker.min.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/bootstrap-timepicker/js/bootstrap-timepicker.min.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/bootstrap-datetimepicker/js/bootstrap-datetimepicker.min.js" type="text/javascript"></script>
+        <script src="assets/global/plugins/clockface/js/clockface.js" type="text/javascript"></script>
+        <!-- END PAGE LEVEL PLUGINS -->
+        <!-- BEGIN THEME GLOBAL SCRIPTS -->
+        <script src="assets/global/scripts/app.min.js" type="text/javascript"></script>
+        <!-- END THEME GLOBAL SCRIPTS -->
+        <!-- BEGIN PAGE LEVEL SCRIPTS -->
+        <script src="assets/pages/scripts/dashboard.min.js" type="text/javascript"></script>
+        <script src="assets/pages/scripts/table-datatables-responsive.min.js" type="text/javascript"></script>
+        <script src="assets/pages/scripts/profile.min.js" type="text/javascript"></script>
+        <script src="assets/pages/scripts/ui-toastr.min.js" type="text/javascript"></script>
+        <script src="assets/pages/scripts/components-date-time-pickers.min.js" type="text/javascript"></script>
+        <!-- END PAGE LEVEL SCRIPTS -->
+        <!-- BEGIN THEME LAYOUT SCRIPTS -->
+        <script src="assets/layouts/layout4/scripts/layout.min.js" type="text/javascript"></script>
+        <script src="assets/layouts/layout4/scripts/demo.min.js" type="text/javascript"></script>
+        <script src="assets/layouts/global/scripts/quick-sidebar.min.js" type="text/javascript"></script>
+        <script src="assets/layouts/global/scripts/quick-nav.min.js" type="text/javascript"></script>
+        <!-- END THEME LAYOUT SCRIPTS -->
+    </body>
 </html>
